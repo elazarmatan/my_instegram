dcc0b5cfe99bc0c4fcaaa79e0f0915d2421efb89
elazar matan <elazarmata8@gmail.com>
2025-09-07 14:53:47 +0300

    component for logo

---
+++ client/src/components/logo.tsx
@@ -0,0 +1,7 @@
+import '../style/logo.css'
+export default function Logo(){
+    return <header>
+    <h1 className="logoText">LinkKodkod</h1>
+    <img src="../src/assets/images/linkKodkodLogo.png" className="logoImage"/>
+    </header>
+}
