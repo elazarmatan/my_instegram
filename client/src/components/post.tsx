ab160479f7c1ab1e6b70bbc1a6da3a04362bca6d
elazar matan <elazarmata8@gmail.com>
2025-09-07 14:51:52 +0300

    component for one post

---
+++ client/src/components/post.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../style/post.css'
+export default function Post(props:{urlImage:string,description:string,userName:string,dateAndHour:string}){
+    const [likes,setLikes] = useState(0)
+    return<div className="post">
+        <p className='userNamePost'>{props.userName}</p>
+        <img src={props.urlImage} className="imagePost"/>
+        <button className="likes" onClick={()=> {
+            setLikes(prev => prev === 0? prev +1:prev)
+        }}>like</button>
+        <p className='countLikes'>{likes}</p>
+        <p className="descriptPost">{props.description}</p>
+        <p className="datePost">{props.dateAndHour}</p>
+    </div>
+}
