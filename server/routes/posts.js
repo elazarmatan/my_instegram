811bbf826ad724282a0dbbb2c1963e6c24053a82
elazar matan <elazarmata8@gmail.com>
2025-09-07 17:35:14 +0300

    finish mission router posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,27 +1,40 @@
 import express from 'express'
 import getAllPosts from '../controllers/getAllPosts.js'
+import getSpecificPosts from '../controllers/getSpecificPosts.js'
 
 const routerPosts = express.Router()
 
+const path = './db/posts.json'
+
 routerPosts.get('/getAllPosts',async (req,res) => {
     try {
-        const posts = await getAllPosts('./db/posts.json')
+        const posts = await getAllPosts(path)
         res.json(posts)
     } catch (error) {
         res.status(400).json({msg:error})
     }
 })
 
-routerPosts.get('getPostByUserName/:userName',(req,res) => {
+routerPosts.get('/getPostByUserName/:userName',async(req,res) => {
+    console.log(req.params.userName)
     try {
-        
+        const posts = await getSpecificPosts('userName',req.params.userName,path)
+        res.json(posts)
     } catch (error) {
-        
+        res.status(400).json({msg:error})
     }
 })
 
-routerPosts.get('getPostByDateAndHour/:DateAndHour',(req,res) => {
+routerPosts.post('/createPost',async(req,res) => {
+    res.send('hi from create')
+})
 
+routerPosts.put('/updatePost',async(req,res) => {
+    res.send('hi from update')
+})
+
+routerPosts.delete('/deletePost',async(req,res) => {
+    res.send('hi from delete')
 })
 
 export default routerPosts
