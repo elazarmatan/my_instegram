b5bb2d2798638a6d998799582a723349569de710
elazar matan <elazarmata8@gmail.com>
2025-09-09 10:03:30 +0300

    add stop propagation in like button

--- client/src/components/post.tsx
+++ client/src/components/post.tsx
@@ -5,7 +5,8 @@ export default function Post(props:{urlImage:string,description:string,userName:
     return<div className="post" key={props.dateAndHour}>
         <p className='userNamePost'>{props.userName}</p>
         <img src={props.urlImage} className="imagePost"/>
-        <button className="likes" onClick={()=> {
+        <button className="likes" onClick={(e)=> {
+            e.stopPropagation()
             setLikes(prev => prev === 0? prev +1:prev-1)
         }}>like</button>
         <p className='countLikes'>{likes}</p>
