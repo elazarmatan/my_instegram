8a4017d6d9e59cbb86bc39087f0d2bda3060efe0
elazar matan <elazarmata8@gmail.com>
2025-09-09 17:18:25 +0300

    add user router

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express, { json } from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import routerPosts from './routes/posts.js'
+import userRouter from './routes/users.js'
 
 const server = express()
 
@@ -19,5 +20,6 @@ server.use(express.static('images'))
 server.use(json())
 
 server.use('/posts',routerPosts)
+server.use('/users',userRouter)
 
 server.listen(process.env.PORT,() => console.log(`server listening on port:${process.env.PORT}`))
