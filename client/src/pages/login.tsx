29c712204ce602e3524a115638de7ed8ec9fe6f9
elazar matan <elazarmata8@gmail.com>
2025-09-10 09:18:48 +0300

    add checking if password or user name not match

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -7,24 +7,28 @@ export default function Login(){
     const userName=useRef<HTMLInputElement>(null)
     const password=useRef<HTMLInputElement>(null)
     const entry = useLocation()
-    return<>
-    <input type="text" placeholder="user name" ref={userName}/>
-    <input type="password" placeholder="password" ref={password}/>
-    <button onClick={async() => {
+    const [notError,setNotError] = useState(false)
+    const [submit,setSubmit] = useState(false)
+    return<section id="login">
+    <input type="text" placeholder="user name" ref={userName} className="login"/>
+    <input type="password" placeholder="password" ref={password} className="login"/>
+    <button className="login" onClick={async() => {
+        setSubmit(true)
         const user = {userName:userName.current?.value,password:password.current?.value}
-        console.log(entry.state.login)
         if(entry.state.login === 'login'){
             const res = await login(user)
             if(res.ok){
+                setNotError(true)
                 navigate('/home',{state:{userName:userName.current?.value}})
             }
             else{
-                <h1>password or username not match</h1>
+                setNotError(false)
             }
         }
         else if(entry.state.login === 'signin'){
-
+            
         }
     }}>submit</button>
-    </>
+    {submit?(notError?<p></p>:<h1 id='errorPosts'>password or username not match</h1>):<p></p>}
+    </section>
 }
